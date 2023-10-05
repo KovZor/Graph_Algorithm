fe958ebefc05eab970d92b7abe0e356676d27f85
KovZor <kovzor15@gmail.com>
2023-10-05 08:59:50 +0300

    First commit!

---
+++ lab_1/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+#define N 100
+
+using namespace std;
+
+queue <int>myQueue;
+vector <int> adjList[N];
+int bfs_visited[N] = {0};
+int dist[N];
+int n,m;
+void bfs2(int start)
+{
+    bfs_visited[start] = 1;
+    myQueue.push(start);
+
+    while(!myQueue.empty())
+    {
+        int u = myQueue.front();
+        cout << u  << " ";
+        for (int i = 0; i < adjList[u].size(); ++i) {
+            int v = adjList[u][i];
+            if(bfs_visited[v] == 0)
+            {
+                bfs_visited[v] = 1;
+                dist[v] = dist[u] + 1;
+                myQueue.push(v);
+            }
+        }
+        myQueue.pop();
+    }
+}
+
+void read(char *filename)
+{
+    ifstream fin(filename);
+    fin >> n >> m;
+    for (int i = 1; i <= m; ++i) {
+        int u,v;
+        fin >> u >> v;
+        adjList[u ].push_back(v );
+        adjList[v ].push_back(u );
+    }
+}
+
+void printadjlist()
+{
+    for (int i = 1; i <= n; ++i) {
+        cout << i << ": ";
+        for (int j = 0; j < adjList[i].size(); ++j) {
+            cout << adjList[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    read(argv[1]);
+    printadjlist();
+
+
+    cout << endl;
+    bfs2(2);
+    cout << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << i << " " << dist[i]<< endl;
+    }
+
+    return 0;
+}
