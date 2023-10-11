180e814260215d41977fdc7521a5edb87eb131fa
KovZor <kovzor15@gmail.com>
2023-10-11 20:58:27 +0300

    Second lab commit

--- lab_2/cmake-build-debug/cmake_install.cmake
+++ lab_2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/kovzor/Desktop/Coding/Graph Algorithm/lab_2
+# Install script for directory: C:/Users/kovzor/Documents/GitHub/Graph_Algorithm/lab_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/kovzor/Desktop/Coding/Graph Algorithm/lab_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/kovzor/Documents/GitHub/Graph_Algorithm/lab_2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
